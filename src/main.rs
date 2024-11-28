9c8f048ebb8bb31bf526260a0e7925839afe55da
Dasun Pubudumal <pubudumald@gmail.com>
2024-11-28 10:45:07 +0000

    Add Hyper server implementation and update dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,63 @@
-use labwhere::db::init_db;
-use labwhere::models::location_type::LocationType;
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+use http_body_util::Full;
+use hyper::body::Bytes;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response};
+use hyper_util::rt::TokioIo;
 
 // Any module that is imported into here (e.g., `use abc_module;`) has its ancestry as the binary
 // crate. Therefore, any function that is declared in the module (e.g., `abc_module`) under `pub(crate)`
 // visibility can be accessed by the binary crate and NOT the library crate. If the module needs to be accessed
 // by both crates, it needs to be made `pub`. The binary crate depends on the library crate (which has the same
 // name listed in Cargo.toml); because stuff from library crate are imported in line 1 and 2.
-
 #[tokio::main]
-async fn main() -> Result<(), sqlx::Error> {
-    // Another option is to use SqlitePool. A pool gives a bunch of active connections and will
-    //  resolve a connection from the pool when an database operation starts.
-    let mut conn = init_db("sqlite::memory:").await.unwrap();
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
-    sqlx::query("INSERT INTO location_types (id, name) VALUES (?, ?)")
-        .bind(150_i64)
-        .bind("Freezer")
-        .execute(&mut conn)
-        .await?;
+    // Reference: https://hyper.rs/guides/1/server/hello-world/
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
-    let result: Vec<LocationType> =
-        sqlx::query_as::<_, LocationType>("SELECT * FROM location_types")
-            .fetch_all(&mut conn)
-            .await?;
+    // Create a TCPListener and bind it to local port 3000
+    match TcpListener::bind(addr).await {
+        Ok(listener) => {
+            // Loop in and listen to incoming connections
+            println!("Listening to incoming connections at port 3000");
+            loop {
+                let (stream, _) = listener.accept().await?;
 
-    println!("{:?}", result);
+                // TokioIo implements tokio::io traits. TokioIo is from Hyper.
+                // Hyper is used here to accept and access incoming TCP packets.
+                let io = TokioIo::new(stream);
 
-    assert_eq!(result.len(), 1);
-
+                // Spawn an async Tokio task that accepts incoming connections and proxies them
+                // to service functions.
+                tokio::task::spawn(async move {
+                    if let Err(err) = http1::Builder::new()
+                        .serve_connection(io, service_fn(pong))
+                        .await
+                    {
+                        eprintln!("Error service connection: {:?}", err);
+                    }
+                });
+            }
+        }
+        Err(err) => {
+            eprintln!("Error in spinning up the server: {:?}", err);
+        }
+    };
     Ok(())
+}
+
+/// This service function is the root-level service function. 
+/// The root service function should have multiple child service functions.
+/// The root-level service function delegates the processing of incoming messages based on 
+/// the header contents of the incoming message.
+/// Note that the incoming message is anonymised (_), but this should be taken into
+/// consideration when delegating to the child services.  
+/// Also, the main and child services should be in their own module hierarchies.
+async fn pong(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    Ok(Response::new(Full::new(Bytes::from("pong!"))))
 }
